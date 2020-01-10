6e87bb09ef3b62d97ff94d2f220b7ea560061a2c
George Kettleborough <kettleg@gmail.com>
2020-01-10 21:18:45 +0000

    Add flake8 pre-commit hook

--- music_database/music_database/settings.py
+++ music_database/music_database/settings.py
@@ -88,7 +88,10 @@ DATABASES = {"default": dj_database_url.config(conn_max_age=0)}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+        "NAME": (
+            "django.contrib.auth.password_validation."
+            "UserAttributeSimilarityValidator"
+        )
     },
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
